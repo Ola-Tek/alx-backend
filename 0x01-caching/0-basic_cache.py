0fff8a5b28d472f4de13abcf466d038e561ad59d
root <Obisesanolabode10@gmail.com>
2024-07-24 18:38:07 +0000

    caching and eviction policy of cacahing

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Script for a basic module cache
+"""
+from basic_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """it represent an object that allows storing and
+    retrieving objects from a temporary memory like a dictionary.
+    """
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
+
---
+++ 0x01-caching/README.md
@@ -0,0 +1,7 @@
+CACHING SYSTEM:
+A caching system is a mechanism or system that stores data in atemporary location for the sake of retrieving data faster withour going to the primary source or data source.
+Key components of a caching system includes;
+Data Requests
+Cache Lookup which includes: cache hit & cache miss
+cache update
+Eviction policy which includes: LIFO, FIFO, SIEVE, LRU, MRU, LFU, LFRU.
